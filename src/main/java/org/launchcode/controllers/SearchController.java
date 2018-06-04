17c9c007c21c93c691fde5249956ad250e3d89e8
Dylan Mahala <dmmahala@hotmail.com>
2018-06-04 15:23:49 -0500

    finished TechJobs MVC assignment

--- src/main/java/org/launchcode/controllers/SearchController.java
+++ src/main/java/org/launchcode/controllers/SearchController.java
@@ -4,6 +4,7 @@ import org.launchcode.models.JobData;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RequestParam;
 
 import java.util.ArrayList;
@@ -23,5 +24,19 @@ public class SearchController {
     }
 
     // TODO #1 - Create handler to process search request and display results
+
+    @RequestMapping(value = "results", method = RequestMethod.GET)
+    public String results(Model model, @RequestParam String searchType, @RequestParam String searchTerm) {
+        model.addAttribute("columns", ListController.columnChoices);
+        ArrayList<HashMap<String, String>> results;
+        if (searchType.equals("all")) {
+            results = JobData.findByValue(searchTerm);
+        }
+        else {
+            results = JobData.findByColumnAndValue(searchType, searchTerm);
+        }
+        model.addAttribute("jobs", results);
+        return "search";
+    }
 
 }
